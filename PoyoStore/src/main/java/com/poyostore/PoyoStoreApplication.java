6ab722402cc3e5c9a16835fc52bae46a727cb05b
jorgejoshuatt <joshuatrejo18@aragon.unam.mx>
2023-03-21 23:10:12 -0600

    Creación de proyecto y conexión con mongo db (Faltan correcciones con la conexión)

---
+++ PoyoStore/src/main/java/com/poyostore/PoyoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.poyostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoyoStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoyoStoreApplication.class, args);
+    }
+
+}
