7d9e5abafc964de18b19f40eb34caacda7ef5859
Lan Ikari <angel_bazques@outlook.com>
2023-03-23 20:38:22 -0600

    Correccion de version en JDK para la coneccion al gestor de base de datos MongoDB

--- PoyoStore/src/main/java/com/example/poyostore/PoyoStoreApplication.java
+++ PoyoStore/src/main/java/com/example/poyostore/PoyoStoreApplication.java
@@ -1,4 +1,4 @@
-package com.poyostore;
+package com.example.poyostore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,6 @@ public class PoyoStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(PoyoStoreApplication.class, args);
     }
+
 
 }
--- PoyoStore/src/test/java/com/example/poyostore/PoyoStoreApplicationTests.java
+++ PoyoStore/src/test/java/com/example/poyostore/PoyoStoreApplicationTests.java
@@ -1,4 +1,4 @@
-package com.poyostore;
+package com.example.poyostore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
